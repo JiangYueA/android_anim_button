969e949811556358ed3fa511409daa93dca6485c
姜岳 <jiangyue@bestapp.us>
2016-03-25 17:53:13 +0800

    add fill pop btn

---
+++ app/src/main/java/com/anim/button/widget/FillPopImageView.java
@@ -0,0 +1,16 @@
+package com.anim.button.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+/**
+ * Created by jiangyue on 16/3/25.
+ */
+public class FillPopImageView extends ImageView {
+
+    public FillPopImageView(Context context, AttributeSet attrs) {
+        super(context);
+    }
+
+}
