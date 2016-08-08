c9ed56dac93ed35cc91783c2fe4c2b2995dfb9d4
姜岳 <jiangyue@bestapp.us>
2016-08-08 11:03:26 +0800

    add adb

--- app/src/main/java/com/anim/button/MIneApplication.java
+++ app/src/main/java/com/anim/button/MIneApplication.java
@@ -4,6 +4,9 @@ import android.app.Application;
 
 import com.github.mmin18.layoutcast.LayoutCast;
 
+import net.youmi.android.AdManager;
+import net.youmi.android.spot.SpotManager;
+
 /**
  * Created by jiangyue on 16/8/5.
  */
@@ -11,6 +14,13 @@ public class MineApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        //有米
+        AdManager.getInstance(this).init("5027a4067307a270", "db60237986719d51", false);
+        SpotManager.getInstance(this).loadSpotAds();
+        SpotManager.getInstance(this).setSpotOrientation(SpotManager.ORIENTATION_PORTRAIT);
+        SpotManager.getInstance(this).setAnimationType(SpotManager.ANIM_ADVANCE);
+
         LayoutCast.init(this);
     }
 }
--- app/src/main/java/com/anim/button/activity/MainActivity.java
+++ app/src/main/java/com/anim/button/activity/MainActivity.java
@@ -3,12 +3,17 @@ package com.anim.button.activity;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.LinearLayout;
 
 import com.anim.button.R;
 import com.anim.button.adapter.MainAdapter;
 import com.anim.button.util.DisplayUtil;
 import com.anim.button.widget.listview.gesture.GestureListView;
 import com.anim.button.widget.listview.loadmore.PtrClassicFrameLayout;
+
+import net.youmi.android.banner.AdSize;
+import net.youmi.android.banner.AdView;
+import net.youmi.android.spot.SpotManager;
 
 import in.srain.cube.views.ptr.PtrDefaultHandler;
 import in.srain.cube.views.ptr.PtrFrameLayout;
@@ -31,12 +36,33 @@ public class MainActivity extends Activity {
 
         DisplayUtil.init(this);
 
+        AdView adView = new AdView(this, AdSize.FIT_SCREEN);
+        // 获取要嵌入广告条的布局
+        LinearLayout adLayout = (LinearLayout) findViewById(R.id.adLayout);
+        // 将广告条加入到布局中
+        adLayout.addView(adView);
+
         initView();
     }
 
+    @Override
+    public void onBackPressed() {
+        // 如果有需要，可以点击后退关闭插播广告。
+        if (!SpotManager.getInstance(this).disMiss()) {
+            // 弹出退出窗口，可以使用自定义退屏弹出和回退动画,参照demo,若不使用动画，传入-1
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        SpotManager.getInstance(this).onDestroy();
+        super.onDestroy();
+    }
+
     private void initView() {
-        listView  = (GestureListView) findViewById(R.id.lv_content);
-        ptrly= (PtrClassicFrameLayout) findViewById(R.id.ptrlyt_load);
+        listView = (GestureListView) findViewById(R.id.lv_content);
+        ptrly = (PtrClassicFrameLayout) findViewById(R.id.ptrlyt_load);
 
         adapter = new MainAdapter(this);
         listView.setAdapter(adapter);
